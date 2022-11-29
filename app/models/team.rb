0ea3b92d93ad0ba07c031ddd1fe69595cc6c62a0
Julien Fereira <ferreira-julien@live.fr>
2022-11-29 12:58:52 +0100

    routes

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,8 @@ class Team < ApplicationRecord
   belongs_to :match
   has_many :participations
   validates :name, presence: true, length: { in: 3..15 }
+
+  def count_validate?
+    participations.length < 3
+  end
 end
