29ade5d742d734d3c9f9b6837539faa1f2ad64bc
joel irmiahu <joelirmiahu66@gmail.com>
2022-11-29 15:32:09 +0100

    feur

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,12 +3,20 @@ class MatchesController < ApplicationController
   end
 
   def show
+    @match = Match.find(params[:id])
+   #@booking = Booking.new
   end
 
   def new
+    @match = Match.new
+
   end
 
   def create
+    @match = Match.new(match_params)
+    @match.user = current_user
+    @match.save
+    redirect_to match_path(@match)
   end
 
   def update
