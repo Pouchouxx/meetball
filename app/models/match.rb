7781633a82351d95eca05564c7d621889346bbd9
raphael assaf <raphassaf01@outlook.com>
2022-11-29 12:24:12 +0100

    dependent: :destroy

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,7 +1,7 @@
 class Match < ApplicationRecord
   belongs_to :user
-  has_many :teams
-  has_many :messages
+  has_many :teams, dependent: :destroy
+  has_many :messages, dependent: :destroy
   validates :date, presence: true, if: :date_now?
   validates :comment, presence: true, length: { in: 20..100 }
 
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
   belongs_to :match
-  has_many :participations
+  has_many :participations, dependent: :destroy
   validates :name, presence: true, length: { in: 3..15 }
 end
