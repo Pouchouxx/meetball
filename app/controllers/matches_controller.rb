a30527fd0ad651d172f130715b7342425a8c90d1
joel irmiahu <joelirmiahu66@gmail.com>
2022-11-30 14:56:59 +0100

    index

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,5 @@
 class MatchesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index ]
   def index
     @matches = Match.all
     @markers = @matches.geocoded.map do |match|
