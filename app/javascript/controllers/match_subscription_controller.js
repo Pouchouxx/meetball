bd1b767676aec48f8a6eb848ae823eb0cde32715
Julien Fereira <ferreira-julien@live.fr>
2022-12-05 17:29:55 +0100

    chat

--- app/javascript/controllers/match_subscription_controller.js
+++ app/javascript/controllers/match_subscription_controller.js
@@ -2,23 +2,44 @@ import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
-  static values = { matchId: Number }
+  static values = { matchId: Number, currentUserId: Number }
   static targets = ["messages"]
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "MatchChannel", id: this.matchIdValue },
       { received: data => this.#insertMessageAndScrollDown(data)}
-      )
-      console.log(`Subscribed to the chatroom with the id ${this.matchIdValue}.`)
-    }
-
-    #insertMessageAndScrollDown(data) {
-      this.messagesTarget.insertAdjacentHTML("beforeend", data)
-      this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
-    }
-    resetForm(event) {
-      console.log("coucou")
-      event.target.reset()
-    }
+    )
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
+
+  #insertMessageAndScrollDown(data) {
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id;
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  resetForm(event) {
+    event.target.reset()
+  }
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
+  }
+}
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,9 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :match
   validates :content, presence: true, length: { maximum: 150 }
+
+  def sender?(a_user)
+    user.id == a_user.id
+  end
+
 end
