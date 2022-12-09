1e2703b59f9af04cb0b08cb76463b0720bb52734
raphael assaf <raphassaf01@outlook.com>
2022-12-09 10:01:15 +0100

    my games

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :votes
-  has_many :matches
+  has_many :matches # les matches aue l'utilisateur a cree
   has_many :participations, dependent: :destroy
+  has_many :matches_as_participant, through: :participations, source: :match # les matches auquel l'utilisateur participe
   has_many :messages
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
